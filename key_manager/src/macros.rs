ea914aa6cb08a82796890a6630b7d0e802aabdf1
Noah Stiltner <nstilt1@lsu.edu>
2024-08-07 17:41:07 -0500

    got rid of a warning

--- key_manager/src/macros.rs
+++ key_manager/src/macros.rs
@@ -72,8 +72,8 @@ macro_rules! impl_handle_crypto {
             request_bytes: &[u8],
             is_handshake: bool,
             signature: Vec<u8>
-        ) -> Result<($crate::Response, $signature_type), ApiError> {
-            let mut request = $crate::Request::decode_length_delimited(request_bytes)?;
+        ) -> Result<($crate::generated::Response, $signature_type), ApiError> {
+            let mut request = $crate::generated::Request::decode_length_delimited(request_bytes)?;
             let chosen_symmetric_algorithm = request.symmetric_algorithm.to_lowercase();
             match chosen_symmetric_algorithm.as_str() {
                 $(
