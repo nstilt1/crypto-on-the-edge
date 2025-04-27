ea5eab0b5c8ed1c0bdfc05926d155a25d5010079
Noah Stiltner <nstilt1@lsu.edu>
2025-04-26 23:03:28 -0500

    added some benches

--- benches/src/key_manager.rs
+++ benches/src/key_manager.rs
@@ -1,7 +1,7 @@
 use chacha20poly1305::ChaCha20Poly1305;
-use criterion::{Criterion, BenchmarkId, criterion_group, criterion_main};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 type Benchmarker = Criterion;
-use http_private_key_manager::{prelude::{KeyGenerator, *}, private_key_generator::{hkdf::hmac::Hmac, timestamp_policies}};
+use http_private_key_manager::{prelude::{KeyGenerator, *}, private_key_generator::{hkdf::hmac::Hmac, timestamp_policies, CryptoKeyGenerator}};
 
 
 type BigId = BinaryId<U48, U8, 6, timestamp_policies::use_timestamps::Sometimes>;
@@ -49,6 +49,7 @@ fn bench(c: &mut Benchmarker) {
     group.bench_function(BenchmarkId::new("init", 1), |b| {
         b.iter(||init_key_manager());
     });
+    /*
     let mut group = c.benchmark_group("decrypt");
     group.bench_function(BenchmarkId::new("decrypt", 1), |b| {
         let mut key_manager = init_key_manager();
@@ -56,8 +57,37 @@ fn bench(c: &mut Benchmarker) {
             key_manager.decrypt_and_hash_request::<ChaCha20Poly1305, Sha256, _>(request, request_bytes, false)
         });
     });
-    let mut group = c.benchmark_group("generate_id");
-    let mut group = c.benchmark_group("encrypt");
+    */
+    let mut key_manager = init_key_manager();
+    group.bench_function(BenchmarkId::new("generate_keyless_id", 1), |b| {
+        b.iter(|| key_manager.generate_keyless_id::<BigId>("prefix", b"test", None, None).unwrap());
+    });
+    
+    group.bench_function(BenchmarkId::new("generate_ECDSA_key_id", 1), |b| {
+        b.iter(|| key_manager.generate_ecdsa_key_and_id::<NistP384, BigId>("prefix", None, None).unwrap());
+    });
+
+    group.bench_function(BenchmarkId::new("generate_ECDH_key_id", 1), |b| {
+        b.iter(|| key_manager.key_generator.generate_ecdh_pubkey_and_id::<NistP384, BigId>(b"prefix", None, None, &mut key_manager.rng).unwrap());
+    });
+
+    let id = key_manager.generate_ecdsa_key_and_id::<NistP384, BigId>("prefix", None, None).unwrap();
+    group.bench_function(BenchmarkId::new("validate_ECDSA_key_id", 1), |b| {
+        b.iter(|| {
+            let result = key_manager.validate_ecdsa_key_id::<NistP384, BigId>(&id.0.encoded_id, None);
+            black_box(result.is_ok())
+        });
+    });
+
+    group.bench_function(BenchmarkId::new("sign_with_key_id", 1), |b| {
+        b.iter(|| {
+            let result = key_manager.sign_data_with_key_id::<NistP384, BigId, Sha256>(&[5u8; 1024], &id.0);
+            black_box(result.is_ok())
+        });
+    });
+    
+
+    //let mut group = c.benchmark_group("encrypt");
 }
 
 criterion_group!(benches, bench);
